833deae42d75d85ba5f32da570a890239f2cd49f
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2022-10-11 12:15:41 +0200

    :sparkles: Changed Subject to BehaviorSubject in DataService

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Subject, Subscription } from 'rxjs';
+import { BehaviorSubject, Subject, Subscription } from 'rxjs';
 
 export enum Status {
   NEW,
@@ -23,7 +23,7 @@ export class Mock {
 export class DataService {
   static ENDPOINT_URL = 'https://633dfa8283f50e9ba3a9fc10.mockapi.io/Mock';
 
-  dataSubject = new Subject<Mock[]>();
+  dataSubject = new BehaviorSubject<Mock[]>([]);
 
   constructor(private httpClient: HttpClient, private snackBar: MatSnackBar) { }
 
@@ -33,7 +33,7 @@ export class DataService {
         this.dataSubject.next(
           result.map(entry => ({ ...entry, status: Status.UNCHANGED }))
         );
-        
+
         if (callback) callback();
       },
       error: () => this.onError()
